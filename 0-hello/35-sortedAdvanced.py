998a1a3001d74d40cb2b858cff225fa031c383c0
Elena Pontecchiani <ele.pontecchiani@gmail.com>
2020-01-27 19:28:58 +0100

    advanced SORTED function in list

---
+++ 0-hello/35-sortedAdvanced.py
@@ -0,0 +1,16 @@
+#format=(nome, raggio, denisità, distanza dal sole)
+planets =[
+    ("Mercury", 2440, 5.43, 0.395),
+    ("Venus", 6052, 5.24, 0.723),
+    ("Earth", 6378, 5.52, 1.000),
+    ("Mars", 3396, 3.93, 1.530),
+    ("Jupiter", 71492, 1.33, 5.210),
+    ("Saturn", 60268, 0.69, 9.551),
+    ("Uranus", 25559, 1.27, 19.213),
+    ("Neptune", 24764, 1.64, 30.070),
+]
+
+#se voglio riordinare per vari parametri, faccio una f che torna il risultato dell'op di sort
+size = lambda planet: planet[1]
+planets.sort(key = size, reverse=True)
+print(planets)
