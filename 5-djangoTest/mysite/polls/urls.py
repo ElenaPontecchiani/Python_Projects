3d8fd67c41c532216374ce3420082ad70fbbdb59
Elena Pontecchiani <ele.pontecchiani@gmail.com>
2020-02-01 12:43:57 +0100

    aggiunta template html, aggiornamento viste, url dinamici, aggionramento model

--- 5-djangoTest/mysite/polls/urls.py
+++ 5-djangoTest/mysite/polls/urls.py
@@ -6,5 +6,12 @@ from . import views #perchè richiamiamo le funzioni che definiamo nelle view
 urlpatterns = [
     #VUOL DIRE: SE QUALCUNO VA SUL NOSTRO SITO CHE SI CHIAMA, PER ESEMPIO, ELENA.COM, SENZA NIENTE (QUINDI SOLO ELENA.COM) VIENE INDIRIZZATO DALLA FUNZIONE INDEX DEL FILE VIEWS
     #NEL NOSTRO CASO: http://www.localhost:8000/polls/ SENZA NIENTE CHE SEGUE POLLS
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- 5-djangoTest/mysite/polls/views.py
+++ 5-djangoTest/mysite/polls/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from .models import Question
+
+#per importare template html
+from django.template import loader
+#per shortcut con render
+from django.shortcuts import get_object_or_404, render
+
+#per gestione err 404
+from django.http import Http404
+
 
 #LA FUNZIONE DICE CHE PAGINA VISUALIZZARE QUANDO VADO ALL'HOMEPAGE
 #PER MAPPARE OGNI VISTA CON UNO SPECIFICO URL (NEL NOSTRO CASO http://localhost:8000/polls/)
@@ -6,4 +16,26 @@ from django.http import HttpResponse
 #NELLA CARTELLA mysite/mysite
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    #il segno meno in  -pub_date indica che l'ordine in cui le prendo è inverso, non c'entra con la sinatssi vera e propria della richiesta al db
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #constesto: racchiude tutte le var, aassociate a un nome, che voglio passare al mio file html. Vedi più info in commenti index.html
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+
+#in queste f come parametro passo anche question_id, perchè riferiscono a uno specifico el del db
+#mentre il parametro request esiste sempre in qualsiasi richiesta!!
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
